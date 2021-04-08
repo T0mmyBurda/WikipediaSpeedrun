8e5e078a374d8b7831a2c9fa78bcdbee5e1221ec
T0mmyBurda <31544632+T0mmyBurda@users.noreply.github.com>
2021-04-07 21:16:31 -0400

    Game now has randomized start and end points
    Also fixed bug with input of next page

--- SpeedrunGame.py
+++ SpeedrunGame.py
@@ -1,17 +1,29 @@
 from wikipedia import *
 
-ny = wikipedia.page("New York (state)")
-print(ny.links)
+#ny = wikipedia.page("New York (state)")
+#print(ny.links)
+
+pages = ["New York (state)" , "Yellowstone National Park" , "John Green (author)" , "Watchmen" , "Cheetos" ,
+         "St. Barnabas Episcopal Church (Troy, New York)" , "Aphanius baeticus" , "Python (programming language)" ,
+         "Ice pop"]
+
+#print("here")
+print(wikipedia.random(pages=1))
 
 def playGame():
     steps = 0
 
     curPage = ""
 
-    print("Enter the start page: ")
-    startPage = input()
-    print("Enter the end page: ")
-    endPage = input()
+    #print("Enter the start page: ")
+    #startPage = input()
+    #print("Enter the end page: ")
+    #endPage = input()
+
+    startPage = wikipedia.random(pages=1)
+    endPage = wikipedia.random(pages=1)
+
+    print("Get from " , startPage  , " to " , endPage)
 
     curPage = startPage
 
@@ -26,10 +38,10 @@ def playGame():
 
         print("ENTER THE NUMBER OF YOUR NEXT MOVE")
         inNum = input()
-        curPage = links[inNum]
+        curPage = links[int(inNum)]
         steps += 1
 
-    print("congratuations! you got from " , startWord , " to " , endWord , " in " , steps , " steps")
+    print("congratuations! you got from " , startPage , " to " , endPage , " in " , steps , " steps")
 
 
 
