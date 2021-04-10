ea0e54992d26c22c2d325b834301481e4dc9d376
T0mmyBurda <31544632+T0mmyBurda@users.noreply.github.com>
2021-04-09 22:15:13 -0400

    beginning to implement UI with Tkinter

---
+++ GUITest.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+window = Tk()
+window.geometry("300x300")
+window.title("WHOOOHOOOO")
+
+label1 = Label(window , text = "Stop the rain" , bg = "yellow").pack()
+
+window.mainloop()
--- SpeedrunGame.py
+++ SpeedrunGame.py
@@ -1,27 +1,28 @@
 from wikipedia import *
+from tkinter import *
 
-#ny = wikipedia.page("New York (state)")
-#print(ny.links)
+window = Tk()
+window.geometry("400x600")
+window.title("Wikipedia Speedrun")
+
+GAMEFONT = "arial"
 
 pages = ["New York (state)" , "Yellowstone National Park" , "John Green (author)" , "Watchmen" , "Cheetos" ,
          "St. Barnabas Episcopal Church (Troy, New York)" , "Aphanius baeticus" , "Python (programming language)" ,
          "Ice pop"]
 
-#print("here")
-print(wikipedia.random(pages=1))
+#print(wikipedia.random(pages=1))
 
 def playGame():
     steps = 0
 
     curPage = ""
 
-    #print("Enter the start page: ")
-    #startPage = input()
-    #print("Enter the end page: ")
-    #endPage = input()
-
     startPage = wikipedia.random(pages=1)
     endPage = wikipedia.random(pages=1)
+
+    banner = Label(window , text = startPage + " to " + endPage , bg="green")
+    banner.pack(fill = BOTH)
 
     print("Get from " , startPage  , " to " , endPage)
 
@@ -31,7 +32,9 @@ def playGame():
         page = wikipedia.page(curPage)
         links = page.links
 
-        print("ENTER THE NUMBER OF YOUR NEXT MOVE")
+        #clear()
+        #print("Get from " , startPage  , " to " , endPage , "/n/n")
+        print("HERE ARE ALL THE LINKS ON PAGE " , curPage , "/n")
 
         for num , link in enumerate(links):
             print(num , ":  " , link)
@@ -43,6 +46,14 @@ def playGame():
 
     print("congratuations! you got from " , startPage , " to " , endPage , " in " , steps , " steps")
 
-
+def clear():
+  
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+  
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
 
 playGame()
