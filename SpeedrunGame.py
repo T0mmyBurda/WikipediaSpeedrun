4b772fd2f482463a5e681aba8d6e93301dcc6c60
T0mmyBurda <31544632+T0mmyBurda@users.noreply.github.com>
2021-04-12 18:47:03 -0400

    improved basic UI

--- SpeedrunGame.py
+++ SpeedrunGame.py
@@ -17,27 +17,20 @@ pages = ["New York (state)" , "Yellowstone National Park" , "John Green (author)
          "St. Barnabas Episcopal Church (Troy, New York)" , "Aphanius baeticus" , "Python (programming language)" ,
          "Ice pop"]
 
-#print(wikipedia.random(pages=1))
-
 def playGame():
-    steps = 0
 
+    steps = 0
     curPage = ""
 
     startPage = wikipedia.random(pages=1)
     endPage = wikipedia.random(pages=1)
 
-    banner = Label(window , text = startPage + " to " + endPage , bg=wikiBlue , fg="black" , font=(GAMEFONT , 20))
-    banner.pack(fill = BOTH)
+    print(startPage)
 
-    redbutton = Button(window, text="Red", fg="red")
-    greenbutton = Button(window, text="green", fg="green")
-    #redbutton.grid(row = 0, column = 0, sticky = W, pady = 2)
-    #greenbutton.grid(row = 1, column = 0, sticky = W, pady = 2)
-    redbutton.pack()
-    greenbutton.pack()
+    banner = Label(window , text = startPage + " to " + endPage , bg=wikiBlue , fg="black" , font=(GAMEFONT , 20) , width=45)
+    banner.grid(row=0 , column=0 , columnspan=3)
 
-    mainloop()
+    #mainloop()
 
     print("Get from " , startPage  , " to " , endPage)
 
@@ -47,20 +40,31 @@ def playGame():
         page = wikipedia.page(curPage)
         links = page.links
 
-        #clear()
-        #print("Get from " , startPage  , " to " , endPage , "/n/n")
-        print("HERE ARE ALL THE LINKS ON PAGE " , curPage , "/n")
+        #print("HERE ARE ALL THE LINKS ON PAGE " , curPage , "/n")
 
         for num , link in enumerate(links):
             print(num , ":  " , link)
 
-        #b1 = Label(window , text = "text1" , bg=offWhite , fg="black" , font=(GAMEFONT , 20))
-        #b1.pack(fill = X)
-        #b2 = Label(window , text = "text2" , bg="white" , fg="black" , font=(GAMEFONT , 20))
-        #b2.pack(fill = X)
-
         even = True #indexes at 0
-        #while(len(links) >= 3):
+        numCol = 3
+        numRows = len(links) // numCol
+
+        linkMat = []
+        
+        linkNum = 0
+        for i in range(numRows):
+            row = []
+            for j in range(3):
+                nextButton = 'Button(window , text="' + links[linkNum] + '" , padx=0 , pady=0 , width=15)'
+                row.append(eval(nextButton))
+                linkNum += 1
+            linkMat.append(row)
+
+        print(linkMat)
+
+        for row in range(numRows):
+            for col in range(3):
+                linkMat[row][col].grid(row=row + 1 , column=col)
 
 
         print("ENTER THE NUMBER OF YOUR NEXT MOVE")
