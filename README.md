751c79af714f0f7308102b33ceb0addce9b606ef
T0mmyBurda <31544632+T0mmyBurda@users.noreply.github.com>
2021-04-13 16:10:05 -0400

    Updated ReadMe and am beginning to use GUI input

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # WikipediaSpeedrun
+
+ ## About:
  This repo uses the Wikipedia API to make a game where you must get from one topic to another using each pages links
 
+ ## Process
  Currently this repo only has a basic console application version of the game.
  Later goals include:
   * making the application look nicer in console and improving the UI
   * replacing the inputed start and end with a random two preselected pages
   * adding a saved high score functionality using a matrix
   * creating a running graph of the links of all the pages the user inputs
+
+ ## Running the code
+ To run this code use an up to date version of Python and pip install the "wikipedia" module and "tkinter" module (often installed with Python) 
--- SpeedrunGame.py
+++ SpeedrunGame.py
@@ -17,6 +17,14 @@ pages = ["New York (state)" , "Yellowstone National Park" , "John Green (author)
          "St. Barnabas Episcopal Church (Troy, New York)" , "Aphanius baeticus" , "Python (programming language)" ,
          "Ice pop"]
 
+startPage = ""
+endPage = ""
+curPage = ""
+
+def linkClicked(button):
+    curPage = button
+    print("New Page: " , curPage)
+
 def playGame():
 
     steps = 0
@@ -25,12 +33,8 @@ def playGame():
     startPage = wikipedia.random(pages=1)
     endPage = wikipedia.random(pages=1)
 
-    print(startPage)
-
     banner = Label(window , text = startPage + " to " + endPage , bg=wikiBlue , fg="black" , font=(GAMEFONT , 20) , width=45)
     banner.grid(row=0 , column=0 , columnspan=3)
-
-    #mainloop()
 
     print("Get from " , startPage  , " to " , endPage)
 
@@ -55,7 +59,47 @@ def playGame():
         for i in range(numRows):
             row = []
             for j in range(3):
-                nextButton = 'Button(window , text="' + links[linkNum] + '" , padx=0 , pady=0 , width=15)'
+                nextButton = 'Button(window , text="' + links[linkNum] + '" , padx=0 , pady=0 , width=15 , command=linkClicked(links[linkNum]))'
+                row.append(eval(nextButton))
+                linkNum += 1
+            linkMat.append(row)
+
+        #print(linkMat)
+
+        for row in range(numRows):
+            for col in range(3):
+                linkMat[row][col].grid(row=row + 1 , column=col)
+
+        window.mainloop()
+
+#       print("ENTER THE NUMBER OF YOUR NEXT MOVE")
+#       inNum = input()
+#        curPage = links[int(inNum)]
+#        steps += 1
+
+#   print("congratuations! you got from " , startPage , " to " , endPage , " in " , steps , " steps")
+
+def mainLoop():
+    while(curPage != endPage):
+        page = wikipedia.page(curPage)
+        links = page.links
+
+        #print("HERE ARE ALL THE LINKS ON PAGE " , curPage , "/n")
+
+        for num , link in enumerate(links):
+            print(num , ":  " , link)
+
+        even = True #indexes at 0
+        numCol = 3
+        numRows = len(links) // numCol
+
+        linkMat = []
+        
+        linkNum = 0
+        for i in range(numRows):
+            row = []
+            for j in range(3):
+                nextButton = 'Button(window , text="' + links[linkNum] + '" , padx=0 , pady=0 , width=15 , command=Lambda: linkClicked(links[linkNum]))'
                 row.append(eval(nextButton))
                 linkNum += 1
             linkMat.append(row)
@@ -71,8 +115,6 @@ def playGame():
         inNum = input()
         curPage = links[int(inNum)]
         steps += 1
-
-    print("congratuations! you got from " , startPage , " to " , endPage , " in " , steps , " steps")
 
 def clear():
   
