cfaea3d9f2f9df7a444551770a18c2bad762fc1e
T0mmyBurda <31544632+T0mmyBurda@users.noreply.github.com>
2021-04-27 14:16:42 -0400

    Game is now playable

--- SpeedrunGame.py
+++ SpeedrunGame.py
@@ -1,12 +1,13 @@
 from wikipedia import *
 from tkinter import *
+import random
 
 wikiBlue = "#A1C1FA"
 offWhite = "#F7F6F6"
 linkBlue = "#294DAC"
 
 window = Tk()
-window.geometry("400x600")
+window.geometry("550x600")
 window.title("Wikipedia Speedrun")
 
 linkGrid = Frame(window)
@@ -15,17 +16,27 @@ GAMEFONT = "arial"
 
 pages = ["New York (state)" , "Yellowstone National Park" , "John Green (author)" , "Watchmen" , "Cheetos" ,
          "Yoshi" , "Aphanius baeticus" , "Python (programming language)" , "Frindle" , "Yoda" , "Toaster" ,
-         "Ice pop"]
+         "Ice pop" , "Fast & Furious" , "DreamWorks Animation" , "Car Town" , "Android OS"]
 
-startPage = ""
-endPage = ""
+startPage = random.choice(pages)
+endPage = random.choice(pages)
+
+while(endPage == startPage):
+    endPage = random.choice(pages)
+
+#easy start & end for testing 
+#startPage = "Fast & Furious"
+#endPage = "Car Town"
+
 curPage = ""
 links = []
 linkNum = 0
+steps = 0
 
 def linkClicked(button):
     curPage = button
-    if(curPage = endPage):
+    print("End = " , endPage , "   Cur = " , curPage)
+    if(curPage == endPage):
         gameEnd()
     else:
         print("New Page: " , curPage)
@@ -33,11 +44,17 @@ def linkClicked(button):
 
 def startGame():
 
-    steps = 0
-    curPage = ""
+    #startPage = wikipedia.random(pages=1)
+    #endPage = wikipedia.random(pages=1)
 
-    startPage = wikipedia.random(pages=1)
-    endPage = wikipedia.random(pages=1)
+    #startPage = random.choice(pages)
+    #endPage = random.choice(pages)
+
+    #startPage = "Fast & Furious"
+    #endPage = "Car Town"
+
+    #while(endPage == startPage):
+    #    endPage = random.choice(pages)
 
     banner = Label(window , text = startPage + " to " + endPage , bg=wikiBlue , fg="black" , font=(GAMEFONT , 20) , width=45)
     banner.grid(row=0 , column=0 , columnspan=3)
@@ -50,7 +67,7 @@ def startGame():
         page = wikipedia.page(curPage)
         links = page.links
 
-        #print("HERE ARE ALL THE LINKS ON PAGE " , curPage , "/n")
+        print("HERE ARE ALL THE LINKS ON PAGE " , curPage , "/n")
 
         for num , link in enumerate(links):
             print(num , ":  " , link)
@@ -78,14 +95,9 @@ def startGame():
 
         window.mainloop()
 
-#       print("ENTER THE NUMBER OF YOUR NEXT MOVE")
-#       inNum = input()
-#        curPage = links[int(inNum)]
-#        steps += 1
-
-#   print("congratuations! you got from " , startPage , " to " , endPage , " in " , steps , " steps")
-
 def loadLinks(curPage):
+
+    #steps += 1
 
     page = wikipedia.page(curPage)
     links = page.links
@@ -108,11 +120,12 @@ def loadLinks(curPage):
             linkNum += 1
         linkMat.append(row)
 
-    print(linkMat)
 
     for row in range(numRows):
         for col in range(3):
             linkMat[row][col].grid(row=row + 1 , column=col)
+
+    window.mainloop()
 
 
     print("ENTER THE NUMBER OF YOUR NEXT MOVE")
@@ -120,21 +133,21 @@ def loadLinks(curPage):
     curPage = links[int(inNum)]
     steps += 1
 
-def gameEnd:
+def gameEnd():
     print("You Win, Game has ended!!!")
     window.destroy()
 
-def clear():
-  
-    # for windows
-    if name == 'nt':
-        _ = system('cls')
-  
-    # for mac and linux(here, os.name is 'posix')
-    else:
-        _ = system('clear')
+    endWindow = Tk()
+    endWindow.geometry("200x55")
+    endWindow.title("Wikipedia Speedrun")
 
-def buttonPress():
-    pass
+    banner = Label(endWindow , text = "Congratulations," , bg=wikiBlue , fg="black" , font=(GAMEFONT , 20) , width=45)
+    banner.pack()
+
+    banner = Label(endWindow , text = "You Win!" , bg=wikiBlue , fg="black" , font=(GAMEFONT , 20) , width=45)
+    banner.pack()
+
+    #stepLab = Label(endWindow , text = ("It took you " + str(steps) + " steps") , bg=wikiBlue , fg="black" , font=(GAMEFONT , 20) , width=45)
+    #stepLab.pack()
 
 startGame()
